2c0a749f83d309dfc173d96d63af1de8cd4a530a
Life is Now <keylasan28@yahoo.com>
2020-03-18 18:21:36 -0400

    fixed line 43

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,7 @@
+
+
+
+
 // Dependencies
 // =============================================================
 var express = require("express");
@@ -11,6 +15,8 @@ var PORT = process.env.PORT || 3001;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname,"/public")));
+
 
 
 
@@ -20,53 +26,44 @@ app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
+
+
+//app.get("*", function (req, res) {
+    //res.sendFile(path.join(__dirname, "/public/index.html"));
+//});
 
 
 
 app.get("/api/notes", function (req, res) {
-    fs.readFile("db.json",function(err,data)
+    fs.readFile(__dirname + "/db/db.json", "utf-8", function read(err, data) {
+        console.log(data)
+    });
 });
 
-app.post("/", function (req, res) {
-    fs.File("db.json",function(err,data))
-}
-app.get("/", function (req, res) {
-}
-            // Displays all characters
-            app.get("/api/characters", function (req, res) {
-                return res.json(characters);
-            });
 
-            
 
-            // Create New Characters - takes in JSON input
-            app.post("/api/characters", function (req, res) {
-                // req.body hosts is equal to the JSON post sent from the user
-                // This works because of our body parsing middleware
-                var newCharacter = req.body;
+    // Starts the server to begin listening
+    // =============================================================
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    });
 
-                // Using a RegEx Pattern to remove spaces from newCharacter
-                // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-                newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
 
-                console.log(newCharacter);
 
-                characters.push(newCharacter);
 
-                res.json(newCharacter);
-            });
 
-            // Starts the server to begin listening
-            // =============================================================
-            app.listen(PORT, function () {
-                console.log("App listening on PORT " + PORT);
-            });
+
+
+
+
+
+
+
+
+    
