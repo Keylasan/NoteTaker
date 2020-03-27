db67b380b9ee748d6bc7e23df56327a8d2cd76b9
Life is Now <keylasan28@yahoo.com>
2020-03-27 02:16:46 -0400

    new note added to old notes array

--- Develop/server.js
+++ Develop/server.js
@@ -8,68 +8,48 @@ var fs = require("fs");
 var app = express();
 var PORT = process.env.PORT || 3001;
 
-
 //globals
 var notes = [];
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,"/public")));
-
-
+app.use(express.static(path.join(__dirname, "/public")));
 
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
-
 
 //app.get("*", function (req, res) {
-    //res.sendFile(path.join(__dirname, "/public/index.html"));
+//res.sendFile(path.join(__dirname, "/public/index.html"));
 //});
 
-
-
-app.get("/api/notes", function (req, res) {
-    fs.readFile(__dirname + "/db/db.json", "utf-8", function read(err, data) {
-        res.json(JSON.parse(data));
-    });
+app.get("/api/notes", function(req, res) {
+  fs.readFile(__dirname + "/db/db.json", "utf-8", function read(err, data) {
+    res.json(JSON.parse(data));
+  });
 });
 
-app.post("/api/notes", function (req, res) {
-    var newNote = req.body;
-  
+app.post("/api/notes", function(req, res) {
+  var newNote = req.body;
+  fs.readFile(__dirname + "/db/db.json", "utf-8", function read(err, data) {
+    notes = JSON.parse(data);
     notes.push(newNote);
-  
-    res.json(newNote);
-  });
-  
-
-
-
-    // Starts the server to begin listening
-    // =============================================================
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function read(err, data) {
+console.log("new note added");
     });
+  });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-    
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
